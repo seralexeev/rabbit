e2cb2107d4b73925e7959d85a0eebb66a691a8e8
Sergey <ser.alexeev@gmail.com>
2025-07-03 19:24:40 +1000

    wip

--- workspaces/ros2_ws/run.sh
+++ workspaces/ros2_ws/run.sh
@@ -3,7 +3,4 @@ set -e
 source /opt/ros/jazzy/setup.bash
 source install/setup.bash
 
-ros2 interface show rabbit_interfaces/msg/SensorReading
-find install/rabbit_interfaces/lib/ -name '*SensorReading*'
-
 ros2 run rabbit power_sensor
