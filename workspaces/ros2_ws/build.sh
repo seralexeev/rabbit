35794ec515e5662eccdc6b22a555f13caf9ec0c7
Sergey <ser.alexeev@gmail.com>
2025-07-04 12:37:25 +1000

    drop ros2

--- workspaces/ros2_ws/build.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-docker build -t ros2_ws .
-docker create --name extract ros2_ws
-docker cp extract:/ros2_ws/install ./install
-docker rm extract
--- workspaces/ros2_ws/run.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-source /opt/ros/jazzy/setup.bash
-source install/setup.bash
-
-ros2 run rabbit power_sensor
